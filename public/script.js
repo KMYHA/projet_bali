e454ee15ea5f423e01055d84a08da4d086184505
KMYHA <camillehallereau05@gmail.com>
2023-10-19 15:25:31 +0000

    slider

--- public/script.js
+++ public/script.js
@@ -1,15 +1,33 @@
 const icons = document.getElementById('icons');
-const nav = document.getElementById('nav');
-const links=document.querySelectorAll('nav li');
+const nav = document.querySelector('nav ul');
+const main_menu = document.getElementById('main_menu')
+/******** MENU BURGER ********/
 
-nav.classList.remove("active")
-
-icons.addEventListener("click",()=>{
-    nav.classList.toggle("active")
+icons.addEventListener("click", () => {
+    nav.classList.toggle("js-visibility");
+    icons.classList.toggle("js-toggle-icon");
+    main_menu.classList.toggle("js-main-menu");
 })
 
-links.forEach((link)=>{
-    link.addEventListener('click',()=>{
-        nav.classList.remove("active")
-    })
-})
+
+/******** SLIDER ********/
+
+const img_slider = document.querySelectorAll(".js-img-slider")
+let step = 0;
+const next = document.querySelector('.next')
+const previous = document.querySelector('.previous')
+
+function enleverActiveImg() {
+    for (let i = 0; i < img_slider.length; i++) {
+        img_slider[i].classList.remove('active');
+    }
+}
+
+setInterval(function() {
+step++;
+if (step >= img_slider.length) {
+    step = 0
+}
+enleverActiveImg();
+img_slider[step].classList.add('active')
+}, 4000)
